d929dc34e1ebcaecdd052ad6124f5cb715d8316c
Lakindu <lakinduakash@gmail.com>
2020-05-25 16:51:40 +0530

    Add script to run jmeter in container

---
+++ setup-jmeter.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd testcont/jmeter
+docker build . -t jmeter -f jmeter.Dockerfile
+docker rm jmeter-cont
+docker run --mount type=bind,source="$(pwd)"/tests,target=/usr/tests --network host --name jmeter-cont jmeter
+
+
