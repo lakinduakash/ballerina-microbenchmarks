2dc63d6d75f367729b6c47d80ce16b6f3f21bd02
Lakindu <lakinduakash@gmail.com>
2020-07-24 01:18:40 +0530

    Fix comment

--- testcont/jmeter/tests/jmx/runjmxs.sh
+++ testcont/jmeter/tests/jmx/runjmxs.sh
@@ -12,7 +12,7 @@
 /usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory.jmx -Jthreads=5 -Jduration=10 -l results_memory_5.jtl -e -o "reports_memory_5"
 #/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_IO_5.jmx -Jthreads=5 -Jduration=10 -l results_io_5.jtl -e -o "reports_io_5"
 
-# Threads 5
+# Threads 20
 /usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_File.jmx -Jthreads=20 -Jduration=10 -l results_file_5.jtl -e -o "reports_file_20"
 /usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_CPU.jmx -Jthreads=20 -Jduration=10 -l results_cpu_5.jtl -e -o "reports_cpu_20"
 /usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory.jmx -Jthreads=20 -Jduration=10 -l results_memory_5.jtl -e -o "reports_memory_20"
