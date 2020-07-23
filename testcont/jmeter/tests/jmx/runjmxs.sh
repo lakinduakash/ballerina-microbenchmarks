b01e0356d57c14a1779ea0174d0c2aac645b7c63
Lakindu <lakinduakash@gmail.com>
2020-07-24 01:18:18 +0530

    Update parameters

--- testcont/jmeter/tests/jmx/runjmxs.sh
+++ testcont/jmeter/tests/jmx/runjmxs.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_File_5.jmx -Jthreads=5 -Jduration=30 -l results_file_5.jtl -e -o "reports_file_5"
-/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_CPU_5.jmx -l results_cpu_5.jtl -e -o "reports_cpu_5"
-/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory_5.jmx -l results_memory_5.jtl -e -o "reports_memory_5"
-#/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_IO_5.jmx -l results_io_5.jtl -e -o "reports_io_5"
+# Threads 1
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_File.jmx -Jthreads=1 -Jduration=10 -l results_file_1.jtl -e -o "reports_file_1"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_CPU.jmx -Jthreads=1 -Jduration=10 -l results_cpu_1.jtl -e -o "reports_cpu_1"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory.jmx -Jthreads=1 -Jduration=10 -l results_memory_1.jtl -e -o "reports_memory_1"
+#/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_IO_5.jmx -Jthreads=1 -Jduration=10 -l results_io_1.jtl -e -o "reports_io_1"
+
+# Threads 5
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_File.jmx -Jthreads=5 -Jduration=10 -l results_file_5.jtl -e -o "reports_file_5"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_CPU.jmx -Jthreads=5 -Jduration=10 -l results_cpu_5.jtl -e -o "reports_cpu_5"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory.jmx -Jthreads=5 -Jduration=10 -l results_memory_5.jtl -e -o "reports_memory_5"
+#/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_IO_5.jmx -Jthreads=5 -Jduration=10 -l results_io_5.jtl -e -o "reports_io_5"
+
+# Threads 5
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_File.jmx -Jthreads=20 -Jduration=10 -l results_file_5.jtl -e -o "reports_file_20"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_CPU.jmx -Jthreads=20 -Jduration=10 -l results_cpu_5.jtl -e -o "reports_cpu_20"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory.jmx -Jthreads=20 -Jduration=10 -l results_memory_5.jtl -e -o "reports_memory_20"
+#/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_IO_5.jmx -Jthreads=20 -Jduration=10 -l results_io_5.jtl -e -o "reports_io_20"
