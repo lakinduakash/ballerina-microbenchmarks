b4b2ecbb66092b7e59f9acfd7886c4fe4d5b4c5e
Lakindu <lakinduakash@gmail.com>
2020-07-24 01:39:26 +0530

    Add bash variable duration

--- testcont/jmeter/tests/jmx/runjmxs.sh
+++ testcont/jmeter/tests/jmx/runjmxs.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+duration=1600
+
+if $1; then
+    duration=$1
+fi
+
 # Threads 1
 echo "Running tests for 1 thread"
-/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_File.jmx -Jthreads=1 -Jduration=1800 -Jramp_time=3 -l results_file_1.jtl -e -o "reports_file_1"
-/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_CPU.jmx -Jthreads=1 -Jduration=1800 -Jramp_time=3 -l results_cpu_1.jtl -e -o "reports_cpu_1"
-/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory.jmx -Jthreads=1 -Jduration=1800 -Jramp_time=3 -l results_memory_1.jtl -e -o "reports_memory_1"
-#/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_IO_5.jmx -Jthreads=1 -Jduration=1800 -Jramp_time=3 -l results_io_1.jtl -e -o "reports_io_1"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_File.jmx -Jthreads=1 -Jduration=${duration} -Jramp_time=3 -l results_file_1.jtl -e -o "reports_file_1"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_CPU.jmx -Jthreads=1 -Jduration=${duration} -Jramp_time=3 -l results_cpu_1.jtl -e -o "reports_cpu_1"
+/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_Memory.jmx -Jthreads=1 -Jduration=${duration} -Jramp_time=3 -l results_memory_1.jtl -e -o "reports_memory_1"
+#/usr/share/apache-jmeter-5.2.1/bin/jmeter --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_IO_5.jmx -Jthreads=1 -Jduration=${duration} -Jramp_time=3 -l results_io_1.jtl -e -o "reports_io_1"
 echo "#########################"
 
 # Threads 5
