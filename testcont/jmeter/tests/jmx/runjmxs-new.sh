747caee5dbd48e5ae3281314f9167c24a6012af8
Lakindu Akash <lakinduakash@gmail.com>
2020-10-14 02:10:53 +0530

    Update jmeter version

--- testcont/jmeter/tests/jmx/runjmxs-new.sh
+++ testcont/jmeter/tests/jmx/runjmxs-new.sh
@@ -7,7 +7,7 @@ ramp_time=1
 split_time=10
 
 jtl_splitter="java -jar /usr/share/jtl-splitter-0.4.5.jar"
-jmeter="/usr/share/apache-jmeter-5.2.1/bin/jmeter"
+jmeter="/usr/share/apache-jmeter-5.3/bin/jmeter"
 
 
 # Is Prime
