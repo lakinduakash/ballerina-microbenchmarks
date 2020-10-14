89d05675b06a6989e23bc4a0915672583f6f4c7b
Lakindu Akash <lakinduakash@gmail.com>
2020-10-14 14:37:10 +0530

    Reorder docker build

--- setup-jmeter.sh
+++ setup-jmeter.sh
@@ -7,9 +7,11 @@ if [ ! -d apache-jmeter-5.3 ]; then
     wget https://downloads.apache.org//jmeter/binaries/apache-jmeter-5.3.tgz && tar -xf apache-jmeter-5.3.tgz apache-jmeter-5.3
 fi
 
-docker build . -t jmeter -f jmeter.Dockerfile
+
 docker stop jmeter-cont 2>/dev/null
 docker rm jmeter-cont 2>/dev/null
+
+docker build . -t jmeter -f jmeter.Dockerfile
 docker run --mount type=bind,source="$(pwd)"/tests,target=/usr/tests --network host --name jmeter-cont --cpus="1" -d jmeter
 
 
--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-# cp *.bal testcont/balruntime/benchmarks/
-# cp -r files testcont/balruntime/benchmarks/
-
-# cd testcont/balruntime
-# docker build . -t bal-benchmark -f bal.Dockerfile
-# docker rm bal-benchmark-cont
-# docker run --name bal-benchmark-cont -p 9090:9090 --cpus="0.7" -d bal-benchmark
 
 # Start mysql-server
 docker stop my-mysql-benchmark 2>/dev/null
@@ -35,9 +28,11 @@ if [ ! -f new_tests/benchmark/jar/mysql-connector-java-8.0.19.jar ]; then
     wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar -O new_tests/benchmark/jar/mysql-connector-java-8.0.19.jar
 fi
 
+
+docker stop bal-benchmark-cont 2>/dev/null
+docker rm bal-benchmark-cont 2>/dev/null
+
 cp -rf new_tests/benchmark testcont/balruntime/benchmark
 cd testcont/balruntime
 docker build . -t bal-benchmark -f bal.Dockerfile
-docker stop bal-benchmark-cont 2>/dev/null
-docker rm bal-benchmark-cont 2>/dev/null
 docker run --name bal-benchmark-cont --net="host" -p 9090:9090 --cpus="1" --memory="2g" -d bal-benchmark
