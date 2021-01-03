d9a9b15ee65c3de5ee64f9d3df391246b5e2bd6c
Lakindu Akash <lakinduakash@gmail.com>
2021-01-04 02:19:55 +0530

    Add more tests

--- new_tests/benchmark/src/main/main.bal
+++ new_tests/benchmark/src/main/main.bal
@@ -324,4 +324,105 @@ service microbenchmark on new http:Listener(9090) {
 		}
 		
 	}
+
+	resource function echogetdb(http:Caller caller, http:Request request) returns error? {
+
+		http:Request req = new;
+    	req.addHeader("X-ECHO-CODE", "200");
+
+
+    	var response = nettyEP->get("/get", req) ;
+
+		var params = request.getQueryParams();
+		var id = <string>params.get("id")[0];//<string>params.id
+		var query = "SELECT * FROM emp where id = "+id;
+
+		stream<record{}, error> resultStream = mysqlClient4->query(<@untainted>query);
+
+		record {|record {} value;|}|error? result = resultStream.next();
+
+		error? e = resultStream.close();
+		
+		if (response is http:Response) {
+			response.setTextPayload(<@untainted> io:sprintf("%s", result));
+		}
+
+
+    	if (response is http:Response) {
+			string contentType = response.getHeader("Content-Type");
+			int statusCode = response.statusCode;
+
+			check caller->respond(response);
+			
+		} else {
+			io:println("Error when calling the backend: ",
+			response.detail()?.message);
+
+			http:Response res = new;
+            res.statusCode = 500;
+            res.setPayload(response.detail()?.message);
+
+            check caller->respond(res);
+		}
+		
+	}
+
+
+	resource function echogetcpu(http:Caller caller, http:Request request) returns error? {
+
+		http:Request req = new;
+    	req.addHeader("X-ECHO-CODE", "200");
+
+
+    	var response = nettyEP->get("/get", req);
+
+
+		var params = request.getQueryParams();
+
+		int number=check ints:fromString(params.get("number")[0]);
+		boolean is_prime = false;
+
+		boolean flag=false;
+
+		foreach var i in 2...(number/2) {
+				
+				if (number % i) == 0{
+					flag=true;
+					break;
+				}
+			}
+
+		if(number == 1){
+			is_prime=true;
+		}
+		else {
+        	if (!flag){
+				is_prime=true;
+			}
+			else{
+				is_prime=false;
+			}
+    	}
+
+
+    	if (response is http:Response) {
+			string contentType = response.getHeader("Content-Type");
+			int statusCode = response.statusCode;
+
+			response.setTextPayload(<@untainted> io:sprintf("%b", is_prime));
+			check caller->respond(response);
+			
+		} else {
+			io:println("Error when calling the backend: ",
+			response.detail()?.message);
+
+			http:Response res = new;
+            res.statusCode = 500;
+            res.setPayload(response.detail()?.message);
+            var result = caller->respond(res);
+			
+			check caller->respond(res);
+		}
+		
+	}
 }
--- testcont/jmeter/tests/jmx/base-3-one.sh
+++ testcont/jmeter/tests/jmx/base-3-one.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #Set duration
-duration=300 # 5 minutes
+duration=240 # 5 minutes
 ramp_time=1
 
-split_time=60
+split_time=50
 
 sleep_duration=5
 
@@ -15,13 +15,66 @@ allThreads=(100)
 
 echo "Starting test for thread pool size ${POOL_SIZE}"
 
-echo "Running tests for echo_get"
+# echo "Running tests for echo_get"
+
+# for t in ${allThreads[@]}; do
+
+#     filename=results_${POOL_SIZE}_echoget_${t}.jtl
+
+#     request_path="microbenchmark/echoget"
+
+#     param_1_name="number"
+#     param_1_value="7919"
+
+#     param_2_name="id"
+#     param_2_value="1"
+
+#     echo ${filename}
+
+#     ${jmeter} --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_General_4_params.jmx -Jrequest_path=${request_path} -Jparam_1_name=${param_1_name} -Jparam_1_value=${param_1_value} -Jparam_2_name=${param_2_name} -Jparam_2_value=${param_2_value} -Jthreads=${t} -Jduration=${duration} -Jramp_time=${ramp_time} -l ${filename}
+#     sleep ${sleep_duration}
+#     ${jtl_splitter} -f ${filename} -s -u SECONDS -t ${split_time}
+
+#     rm -f *warmup.jtl
+#     rm -f *measurement.jtl
+# done
+
+# echo "#########################"
+
+# echo "Running tests for echo passthrough"
+
+# for t in ${allThreads[@]}; do
+
+#     filename=results_${POOL_SIZE}_passthrough_${t}.jtl
+
+#     request_path="microbenchmark/passthrough"
+
+#     param_1_name="number"
+#     param_1_value="7919"
+
+#     param_2_name="id"
+#     param_2_value="1"
+
+#     echo ${filename}
+
+#     ${jmeter} --forceDeleteResultFile -n -t /usr/tests/jmx/Test_Plan_General_4_params.jmx -Jrequest_path=${request_path} -Jparam_1_name=${param_1_name} -Jparam_1_value=${param_1_value} -Jparam_2_name=${param_2_name} -Jparam_2_value=${param_2_value} -Jthreads=${t} -Jduration=${duration} -Jramp_time=${ramp_time} -l ${filename}
+#     sleep ${sleep_duration}
+#     ${jtl_splitter} -f ${filename} -s -u SECONDS -t ${split_time}
+
+#     rm -f *warmup.jtl
+#     rm -f *measurement.jtl
+# done
+
+# echo "#########################"
+
+
+echo "Running tests for echogetdb"
 
 for t in ${allThreads[@]}; do
 
-    filename=results_${POOL_SIZE}_echoget_${t}.jtl
+    filename=results_${POOL_SIZE}_echogetdb_${t}.jtl
 
-    request_path="microbenchmark/echoget"
+    request_path="microbenchmark/echogetdb"
 
     param_1_name="number"
     param_1_value="7919"
@@ -41,13 +94,14 @@ done
 
 echo "#########################"
 
-echo "Running tests for echo passthrough"
+
+echo "Running tests for echo echogetcpu"
 
 for t in ${allThreads[@]}; do
 
-    filename=results_${POOL_SIZE}_passthrough_${t}.jtl
+    filename=results_${POOL_SIZE}_echogetcpu_${t}.jtl
 
-    request_path="microbenchmark/passthrough"
+    request_path="microbenchmark/echogetcpu"
 
     param_1_name="number"
     param_1_value="7919"
